ea04321acee372457f9bdc7f2f2a01cb15e0ea48
Raul Aquino <aquinoraul207@gmail.com>
2022-10-08 22:41:03 -0300

    [feature/NHB-005] swagger implementation finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Neighbor HUB api')
+    .setDescription('Api for idealization of the NHUB project')
+    .setVersion('1.0')
+    .addTag('NHUB')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, () => {
+    Logger.log(`[PORT]: ${3000}`);
+  });
 }
 bootstrap();
